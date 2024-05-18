6ba790a1fe0e9a9ca444b858573737774c116eee
Raju <nirbhay.singh06@gmail.com>
2024-05-18 10:49:14 +0530

    Changes done to the controller

--- AzureMVC/AzureMVC/Controllers/HomeController.cs
+++ AzureMVC/AzureMVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AzureMVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page here.";
 
             return View();
         }
